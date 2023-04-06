42758d5c4102ad60f5198296ae16a556e8653162
WayanPande <yande554@gmail.com>
2023-04-06 20:55:57 +0700

    Initial Commit

---
+++ FoundationGroup/FoundationGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationGroupApp.swift
+//  FoundationGroup
+//
+//  Created by I Wayan Pande Putra Yudha on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FoundationGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
